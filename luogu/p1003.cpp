a83a01f4953c530858ea20fe0a9864bce7da0bc2
ReekyStive <reekystive@gmail.com>
2019-08-09 18:25:47 +0800

    P1003 ACed

--- luogu/p1003.cpp
+++ luogu/p1003.cpp
@@ -3,28 +3,32 @@
 
 using namespace std;
 
-int top[20001][20001];
+int a[10001], b[10001], g[10001], k[10001];
 
 int main()
 {
-    int n, a, b, g, k;
+    int n;
     cin >> n;
 
-    int i;
-    for (i = 1; i <= n; i++)
-    {
-        cin >> a >> b >> g >> k;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i] >> b[i] >> g[i] >> k[i];
 
-        for (int x = a; x <= a + g; x++)
-            for (int y = b; y <= b + k; y++)
-                top[x][y] = i;
+    int x, y;
+    cin >> x >> y;
+
+    for (int i = n; i >= 1; i--)
+    {
+        if (a[i] > x || b[i] > y)
+            continue;
+        
+        if (a[i] + g[i] >= x && b[i] + k[i] >= y)
+        {
+            cout << i << endl;
+            return 0;
+        }
     }
 
-    cin >> a >> b;
-    if (top[a][b] == 0)
-        cout << -1 << endl;
-    else
-        cout << top[a][b] << endl;
+    cout << -1 << endl;
     
     return 0;
 }
