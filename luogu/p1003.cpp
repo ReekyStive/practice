f83b3b7538a9ce5d42f62fbbb81b2bd99dbd0fa0
ReekyStive <reekystive@gmail.com>
2019-08-09 17:35:30 +0800

    P1003 MLE

--- luogu/p1003.cpp
+++ luogu/p1003.cpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
+int top[20001][20001];
+
 int main()
 {
     int n, a, b, g, k;
-    int val[201] = {-1};
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    int i;
+    for (i = 1; i <= n; i++)
     {
         cin >> a >> b >> g >> k;
 
+        for (int x = a; x <= a + g; x++)
+            for (int y = b; y <= b + k; y++)
+                top[x][y] = i;
     }
+
+    cin >> a >> b;
+    if (top[a][b] == 0)
+        cout << -1 << endl;
+    else
+        cout << top[a][b] << endl;
+    
     return 0;
 }
