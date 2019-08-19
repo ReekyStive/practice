050e45eca63df865f15dbba3a586075e5f1fcf65
ReekyStive <reekystive@gmail.com>
2019-08-19 18:50:28 +0800

    Organize files (deleted a file)

--- test/template.cpp
+++ test/template.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int main()
 {
     cout << __INT_MAX__ << endl;
-    cout << __LONG_MAX__ << endl;
     cout << __LONG_LONG_MAX__ << endl;
-    cout << sizeof(int) << endl;
-    
+
     return 0;
 }
