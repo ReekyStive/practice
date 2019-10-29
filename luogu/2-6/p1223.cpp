1643d69810e6c671a01748cce2ff8289a22d88e8
ReekyStive <reekystive@gmail.com>
2019-10-29 19:41:37 +0800

    P1223 ACed

--- luogu/2-6/p1223.cpp
+++ luogu/2-6/p1223.cpp
@@ -1,9 +1,48 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
 
+const int max_n = 1000;
+
+struct Person {
+    int num;
+    int t;
+};
+
+bool cmp(Person a, Person b)
+{
+    return a.t < b.t;
+}
+
+Person persons[max_n];
+
 int main()
 {
-    
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        persons[i].num = i + 1;
+        cin >> persons[i].t;
+    }
+
+    sort(persons, persons + n, cmp);
+
+    long long res = 0;
+
+    for (int i = 0; i < n; i++)
+        res += persons[i].t * (n - i - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << persons[i].num << " ";
+    cout << endl;
+
+    cout << setiosflags(ios::fixed);
+    cout << setprecision(2);
+
+    cout << (double)res / n << endl;
+
     return 0;
 }
