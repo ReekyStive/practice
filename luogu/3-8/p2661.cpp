6941923c78be833632cbbc1a718831bf12cae6cf
ReekyStive <reekystive@gmail.com>
2019-11-05 21:54:01 +0800

    Start P2661 (Wrong Program)

---
+++ luogu/3-8/p2661.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+const int kMaxN = 200000;
+int t[kMaxN];
+bool curs[kMaxN];
+
+int solve(int n)
+{
+    for (int i = 0; i < n; i++)
+        curs[i] = i;
+
+    bool end = false;
+    int time = 0;
+
+    while (true) {
+        for (int i = 0; i < n; i++) {
+            curs[i] = t[curs[i]];
+            if (curs[i] == i) {
+                //std::cout << "break! i = " << i << std::endl;
+                end = true;
+                break;
+            }
+        }
+        time++;
+        //std::cout << "time: " << time << std::endl;
+        //for (int i = 0; i < n; i++)
+        //std::cout << curs[i] << " ";
+        //std::cout << std::endl;
+        if (end)
+            break;
+    }
+    return time;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> t[i];
+        t[i]--;
+    }
+
+    std::cout << solve(n) << std::endl;
+
+    return 0;
+}
