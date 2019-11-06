bf232b0a49681625cd74290be0374ae811f6fd68
ReekyStive <reekystive@gmail.com>
2019-11-06 13:10:33 +0800

    P2661 80%AC 20%TLE

--- luogu/3-8/p2661.cpp
+++ luogu/3-8/p2661.cpp
@@ -16,16 +16,11 @@ int solve(int n)
         for (int i = 0; i < n; i++) {
             curs[i] = t[curs[i]];
             if (curs[i] == i) {
-                //std::cout << "break! i = " << i << std::endl;
                 end = true;
                 break;
             }
         }
         time++;
-        //std::cout << "time: " << time << std::endl;
-        //for (int i = 0; i < n; i++)
-        //std::cout << curs[i] << " ";
-        //std::cout << std::endl;
         if (end)
             break;
     }
