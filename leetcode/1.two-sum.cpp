8e8654a6f8dc19c947ae5980a5a30d7d4985d7a3
ReekyStive <reekystive@gmail.com>
2019-11-14 12:45:35 +0800

    Leetcode two-sum Accepted

--- leetcode/1.two-sum.cpp
+++ leetcode/1.two-sum.cpp
@@ -4,12 +4,39 @@
  * [1] Two Sum
  */
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
 // @lc code=start
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        for (int i = 0; i < nums.size(); i++) {
+            int remain = target - nums[i];
+            for (int j = 0; j < nums.size(); j++) {
+                if (j == i)
+                    continue;
+                if (nums[j] == remain)
+                    return vector<int>{i, j};
+            }
+        }
+        return vector<int>{};
     }
 };
 // @lc code=end
 
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    for (int i = 0; i < result.size(); i++)
+        cout << result[i] << " ";
+    cout << endl;
+
+    return 0;
+}
